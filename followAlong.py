79d8b00f143bef7defaeb4ffc65edd0cfd87eb04
ajaydhamanda <ajaydhamanda24@gmail.com>
2023-02-12 23:30:35 +0530

    Create followAlong.py
    followalong

---
+++ followAlong.py
@@ -0,0 +1,15 @@
+'''
+
+while True:
+    greeting = input("Hello, what is your name?")
+    print(greeting.upper() '''
+
+'''
+countries = []
+
+while True:
+    country = input("Enter the country: ")
+    countries.append(country.capitalize())
+    print(countries)
+
+'''
