9f0ca7de3176e79750f51d9684d2460edf2290ae
ajaydhamanda <ajaydhamanda24@gmail.com>
2023-02-13 13:30:32 +0530

    added 2 older projects and added edit and remove functionality for to-do list

--- 1.todo-list.py
+++ 1.todo-list.py
@@ -1,5 +1,5 @@
 
-user_prompt = "Type add, show/display, edit, remove or exit : "
+user_prompt = "Type add, show/display, edit, remove, complete or exit : "
 todos = []
 while True:
     action = input(user_prompt)
@@ -7,13 +7,13 @@ while True:
 
     match action:
         case 'add':
-            todo = input("Enter an item to add to your to do list: ")
+            todo = input("Enter an item to add to your list: ")
             todos.append(todo)
             print(todos)
         case  'show' | 'display':
-            for item in todos:
-                item = item.capitalize()
-                print(item)
+            for index, item in enumerate(todos):
+                row = f"{index + 1}-{item}"
+                print(row)
         case 'exit':
             break
         case 'remove':
@@ -26,6 +26,11 @@ while True:
             number -= 1
             ed_todo = input("Enter the new to-do")
             todos[number] = ed_todo
+        case 'complete':
+            number = int(input("Enter the number of the task completed"))
+            number -= 1
+            todos = todos.pop(number)
+            print(todos)
         case unrecognized:
             print("Sorry, please try again. Type add, show or exit: ")
 print("Here is your to-do list! Do visit again.")
