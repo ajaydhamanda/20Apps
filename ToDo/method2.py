0c6de6cef278b9791d219df698cb470f50ad580f
ajaydhamanda <ajaydhamanda24@gmail.com>
2023-02-14 13:05:38 +0530

    finally fixed a few items. add, show, edit, remove and complete in reference to todo.txt.

--- ToDo/method2.py
+++ ToDo/method2.py
@@ -1,56 +1,3 @@
-'''
-while True:
-    user_action = input("Type add, show/display, edit, remove, complete or exit : ")
-    user_action = user_action.strip()
-
-    if 'add' in user_action:
-       todo = user_action[4:]
-
-    with open('todo.txt', 'r') as file:
-            todos = file.readlines()
-
-    todos.append(todo + '\n')
-
-    with open('todo.txt', 'w') as file:
-          file.writelines(todos)
-
-    if  'show' or 'display' in user_action:
-
-        with open('todo.txt', 'r') as file:
-            todos = file.readlines()
-        #new_todos = [item.strip(\n) for item in todos]
-               new_todos = []
-        for item in todos:
-            new_item = item.strip('\n')
-            new_todos.append(new_item)
-        for index, item in enumerate(todos):
-            item = item.strip('\n')
-            row = f"{index+1}-{item}"
-            print(row)
-
-    if 'exit' in user_action:
-        break
-
-    if 'remove' in user_action:
-        number = int(input("Enter the number of the item to remove: "))
-        number -= 1
-        rem_todo = todos.remove[number]
-        print(rem_todo)
-
-    if 'edit' in user_action:
-        number = int(input("Enter the number of item you want to edit: "))
-        number -= 1
-        ed_todo = input("Enter the new to-do")
-        todos[number] = ed_todo
-
-    if 'complete' in user_action:
-        number = int(input("Enter the number of the task completed"))
-        number -= 1
-        todos = todos.pop([number])
-        print(todos)
-
-'''
-
 while True:
     user_action = input("Type add, show/display, edit, remove, complete or exit: ")
     user_action = user_action.strip()
@@ -131,4 +78,3 @@ while True:
             print(element)
 
         print("Completed!")
-
